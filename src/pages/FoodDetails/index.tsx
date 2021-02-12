f2cea8ebafb700853657ef47265783460eec1448
Willian Oliveira <wijloc@gmail.com>
2021-02-12 12:11:29 -0300

    feat: increment and decrement food quantity

--- src/pages/FoodDetails/index.tsx
+++ src/pages/FoodDetails/index.tsx
@@ -132,10 +132,13 @@ const FoodDetails: React.FC = () => {
 
   function handleIncrementFood(): void {
     // Increment food quantity
+    setFoodQuantity(foodQuantity + 1);
   }
 
   function handleDecrementFood(): void {
     // Decrement food quantity
+    if (foodQuantity === 1) return;
+    setFoodQuantity(foodQuantity - 1);
   }
 
   const toggleFavorite = useCallback(() => {
